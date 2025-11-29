b9042e89113480d917041db7b491f662b5e2dcbc
kawaiicakes <vomitmopper@gmail.com>
2025-11-29 13:33:52 -0700

    get NUKED

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-For Vassal S2
+For Vassal S2.
+
+This should be completely finished now. I don't think I'm even gonna bother publishing this, but to future me, make sure to change
+all this stuff up if I change my mind lmao
--- src/main/java/io/github/kawaiicakes/potionnuke/PotionNuke.java
+++ src/main/java/io/github/kawaiicakes/potionnuke/PotionNuke.java
@@ -1,7 +1,6 @@
 package io.github.kawaiicakes.potionnuke;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -11,11 +10,9 @@ public final class PotionNuke {
 
     public PotionNuke() {
         MinecraftForge.EVENT_BUS.register(this);
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     }
 
     public PotionNuke(FMLJavaModLoadingContext context) {
         MinecraftForge.EVENT_BUS.register(this);
-        final IEventBus modEventBus = context.getModEventBus();
     }
 }
